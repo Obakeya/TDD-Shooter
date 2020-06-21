175cfcd96ef56e9d8afc15aec2de0653bca02599
Obakeya <eamsradiohe@gmail.com>
2020-06-21 20:23:19 +0900

    矢印キーで機体が動作するように実装

--- TDD_Shooter/MainPage.xaml.cs
+++ TDD_Shooter/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,11 +23,37 @@ namespace TDD_Shooter
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        ViewModel Model;
+        DispatcherTimer timer;
+
         public MainPage()
         {
             this.InitializeComponent();
+            Model = new ViewModel();
+            DataContext = Model;
+            Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
+            Window.Current.CoreWindow.KeyUp += CoreWindow_KeyUp;
 
-            DataContext = new ViewModel();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(20);
+            timer.Tick += Tick;
+            timer.Start();
         }
+
+        private void Tick(object sender , object e)
+        {
+            Model.Tick(1);
+        }
+
+        private void CoreWindow_KeyDown(CoreWindow sender, KeyEventArgs args)
+        {
+            Model.KeyDown(args.VirtualKey);
+        }
+
+        private void CoreWindow_KeyUp(CoreWindow sender , KeyEventArgs args)
+        {
+            Model.KeyUp(args.VirtualKey);
+        }
+
     }
 }
--- TDD_Shooter/Model/Ship.cs
+++ TDD_Shooter/Model/Ship.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,10 +8,32 @@ using Windows.UI.Xaml.Media.Imaging;
 
 namespace TDD_Shooter.Model
 {
-    class Ship
+    class Ship : INotifyPropertyChanged
     {
-        public double  X { set; get; }
-        public double Y { set; get; }
+        public static readonly double Speed = 5;
+
+        private double x, y;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public double X
+        {
+            set
+            {
+                x = value;
+                NotifyPropertyChanged("X");
+            }
+            get { return x; }
+        }
+
+        public double Y
+        {
+            set
+            {
+                y = value;
+                NotifyPropertyChanged("Y");
+            }
+            get { return y; }
+        }
 
         public double Width { get { return 60; } }
         public double Height { get { return 60; } }
@@ -18,6 +41,21 @@ namespace TDD_Shooter.Model
         internal Ship()
         {
             Source = new BitmapImage(new Uri("ms-appx:///Images/ship.png"));
+        }
+
+        internal void Move(double dx, double dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+
+        private void NotifyPropertyChanged(String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+
         }
     }
 }
