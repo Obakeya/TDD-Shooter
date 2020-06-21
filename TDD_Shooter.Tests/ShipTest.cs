44ed5998b24acda2ea4305eefa76322bf4192c93
Obakeya <eamsradiohe@gmail.com>
2020-06-21 18:51:41 +0900

    Shooterクラスのテスト用クラスを作成。UIのテストをするため、属性をUITestMethod として宣言

---
+++ TDD_Shooter.Tests/ShipTest.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+using TDD_Shooter.Model;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace TDD_Shooter.Tests
+{
+    [TestClass]
+    public class ShipTest
+    {
+         const double shipWidth = 60;
+        const double shipHeight = 60;
+
+        [UITestMethod]
+        public void ShipImage()
+        {
+            Ship ship = new Ship();
+            Assert.IsNotNull(ship.Source);
+            Assert.IsInstanceOfType(ship.Source, typeof(BitmapImage));
+        }
+
+        [UITestMethod]
+        public void ShipPos()
+        {
+            Ship ship = new Ship();
+            ship.X = 100;
+            ship.Y = 200;
+            Assert.AreEqual(100, ship.X);
+            Assert.AreEqual(200, ship.Y);
+
+        }
+
+        [UITestMethod]
+        public void ShipSize()
+        {
+            Ship ship = new Ship();
+            Assert.AreEqual(shipWidth, ship.Width);
+            Assert.AreEqual(shipHeight, ship.Height);
+        }
+
+    }
+}
---
+++ TDD_Shooter/Model/Ship.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace TDD_Shooter.Model
+{
+    class Ship
+    {
+        public double  X { set; get; }
+        public double Y { set; get; }
+
+        public double Width { get { return 60; } }
+        public double Height { get { return 60; } }
+        public BitmapImage Source { get; set; }
+        internal Ship()
+        {
+            Source = new BitmapImage(new Uri("ms-appx:///Images/ship.png"));
+        }
+    }
+}
